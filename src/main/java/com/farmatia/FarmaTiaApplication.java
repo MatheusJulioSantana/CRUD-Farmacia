9164e8b004624793646d76f0b9675dbdad409a9b
Matheus <mattzeppelin.mj@gmail.com>
2024-01-04 10:18:13 -0300

    Configurando o projeto

---
+++ src/main/java/com/farmatia/FarmaTiaApplication.java
@@ -0,0 +1,13 @@
+package com.farmatia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaTiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaTiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmatia/FarmaTiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmatia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaTiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
